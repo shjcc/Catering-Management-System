322eb512684caf095084b2081ae50615ec5d210f
Saad Hussain <19938855@students.ltu.edu.au>
2024-10-23 20:28:29 +1100

    modified server.js, is currently running and fetching from port 5001 so make sure everything matches up

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { getOrders, createOrder, updateOrder, deleteOrder } = require('./controllers/orderController');
 const { getInventory, createInventoryItem, updateInventoryItem, deleteInventoryItem } = require('./controllers/inventoryController');
+const {getCustomers, createCustomer, updateCustomer, deleteCustomer} = require('./controllers/crmController');
 
 const app = express();
 app.use(cors());
@@ -9,7 +10,7 @@ app.use(express.json());
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Order Routes
 app.get('/api/orders', getOrders);
