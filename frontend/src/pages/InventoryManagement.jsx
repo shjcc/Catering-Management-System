75480a7df16bb9cb19dd19d5508f7d465c7e05ec
Saad Hussain <19938855@students.ltu.edu.au>
2024-10-26 13:32:40 +1100

    typo...T_T

--- frontend/src/pages/InventoryManagement.jsx
+++ frontend/src/pages/InventoryManagement.jsx
@@ -1,6 +1,6 @@
 import '../styles/Inventory.css';
 import React, { useEffect, useState } from 'react'; 
-import { fetchIngredients, addIngredient, updateIngredient, deleteIngredient } from '..components/Inventory';
+import { fetchIngredients, addIngredient, updateIngredient, deleteIngredient } from '../components/Inventory.jsx';
 
 const InventoryManagement = () => {
   const [ingredients, setIngredients] = useState([]);
